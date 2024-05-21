082ed2d2b94ab90956f0da7d27c7f42e7c0320a5
ADABetti <alice.bet@hotmail.com>
2024-05-21 18:54:49 -0300

    Upload dos exerícios da lista 5

---
+++ Listas/Lista05/ex01/js/script.js
@@ -0,0 +1,6 @@
+let username = window.prompt("Enter your name: ");
+var age = window.prompt("Enter your age: ");
+var job = window.prompt("Enter your job: ");
+var address = window.prompt("Enter your address: ");
+
+window.alert(`Your name is ${username} \nYou are ${age} \nYour current job is ${job} \nYou live on ${address}.`);
---
+++ Listas/Lista05/ex02/ex02.js
@@ -0,0 +1,6 @@
+var username =  "Noah Hunt";
+var age = 34;
+var job = "scientist";
+var address = "33 Orchard Road";
+
+console.log(`Your name is ${username} \nYou are ${age} \nYou work as a ${job} \nYou live on ${address}`);
